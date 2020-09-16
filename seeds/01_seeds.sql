57e7560cbae2269ab8ae1931a74719a964f640a0
James Brown <jamesbrown5292@gmail.com>
2020-09-16 19:22:08 +0000

    add first seeds for users data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES (1, 'James Brown', 'bassdata@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users VALUES (2, 'DeAngelo Vickers', 'juggles@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users VALUES (3, 'Michael Scarn', 'threatlevelmidnight@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users VALUES (4, 'Phyllis Vance', 'refrigerators@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
